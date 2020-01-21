82e0c86f278ef1e4011d24d617c1449f6ff40b6b
cdcummings10 <ccummings0913@gmail.com>
2020-01-21 10:42:58 -0800

    initial commit. Changed from .net Core to .net framework due to designer not being out for .net Core.

--- Soundboard/Soundboard/Form1.cs
+++ Soundboard/Soundboard/Form1.cs
@@ -4,18 +4,27 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WMPLib;
 
 namespace Soundboard
 {
     public partial class Form1 : Form
     {
+        private WindowsMediaPlayer _player;
         public Form1()
         {
             InitializeComponent();
+            _player = new WindowsMediaPlayer();
+            _player.URL = "Megalovania [Electro Swing].mp3";
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _player.controls.play();
+        }
     }
 }
